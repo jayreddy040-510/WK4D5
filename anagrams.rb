f24f1ddad3887dff50bbb33c07565d56232df9bb
Jay Reddy <jayreddy@gmail.com>
2022-07-09 16:10:46 -0700

    finished ph2 of anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -1,12 +1,34 @@
 def first_anagram?(str1, str2)
 
-    arr = []
-    new_str = ""
+    anagrams = str1.split("").permutation.to_a
 
-    str1.each_char.with_index do |x,i|
-        
-        
-        
-    end
+    anagrams.include?(str2.split(""))
+
     
 end
+
+# p first_anagram?("gizmo", "sally")    #=> false
+# p first_anagram?("elvis", "lives")    #=> true
+
+
+#The time complexity of this solution is O(n!) If you increase the size of the strings, runtime elongates to unmanageable spans of time very quickly
+
+def second_anagram?(str1,str2)
+    str2 = str2.split("")
+    str1.split("").each_with_index do |x,i|
+        if str2.index(x)
+            str2.delete_at(str2.index(x))
+        else 
+            return false
+        end
+    end
+
+    return true
+
+end
+
+p second_anagram?("gizmo", "sally")    #=> false
+p second_anagram?("elvis", "lives")    #=> true
+
+
+#first_anagram? is longer time complexity than #second_anagram? the 2nd is O(n)
