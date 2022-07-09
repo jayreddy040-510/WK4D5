34167d45048cf241efd8653c7b2a4395544fc5ee
Jay Reddy <jayreddy@gmail.com>
2022-07-09 16:21:49 -0700

    finished anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -27,8 +27,40 @@ def second_anagram?(str1,str2)
 
 end
 
-p second_anagram?("gizmo", "sally")    #=> false
-p second_anagram?("elvis", "lives")    #=> true
+# p second_anagram?("gizmo", "sally")    #=> false
+# p second_anagram?("elvis", "lives")    #=> true
 
 
 #first_anagram? is longer time complexity than #second_anagram? the 2nd is O(n)
+
+
+def third_anagram?(str1,str2)
+    str1 = str1.split("").sort
+    str2 = str2.split("").sort
+    str1 == str2
+end
+
+# p third_anagram?("gizmo", "sally")    #=> false
+# p third_anagram?("elvis", "lives")    #=> true
+
+#I believe this problem is also O(n) time complexity. You're iterating through each string and then checking if they're equivalent.
+
+def fourth_anagram?(str1, str2)
+
+    str1.split("").tally == str2.split("").tally
+end
+
+# p fourth_anagram?("gizmo", "sally")    #=> false
+# p fourth_anagram?("elvis", "lives")    #=> true
+
+#I believe this problem is ALSO O(n) time complexity, because although we are using hashmaps, I imagine the Array#tally method iterates through each element of an array to create the hash counters for each char.
+
+def bonus_anagram?(str1,str2)
+
+    str = str1+str2
+   str.split("").tally.values.all? {|x| x == 2}
+    
+end
+
+p bonus_anagram?("gizmo", "sally")    #=> false
+p bonus_anagram?("elvis", "lives")    #=> true
