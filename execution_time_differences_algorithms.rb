988b120a4565412f53995d10b3ad36fb0ce92b59
Jay Reddy <jayreddy@gmail.com>
2022-07-09 14:46:12 -0700

    still stuck on contiguous subsum last part, got some code down

--- execution_time_differences_algorithms.rb
+++ execution_time_differences_algorithms.rb
@@ -1,4 +1,4 @@
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 def quadratic_my_min(arr)
     
     arr.each do |x|
@@ -11,9 +11,9 @@ def quadratic_my_min(arr)
 
 end
 
-p quadratic_my_min(list)
+# p quadratic_my_min(list)
 
-
+#Time complexity of this function should be O(n^2)
 
 
 
@@ -30,6 +30,63 @@ def linear_my_min(arr)
 
 end
 
-p linear_my_min(list)  # =>  -5
+# p linear_my_min(list)  # =>  -5
 
 #Time complexity of this function should be O(n)
+
+
+def largest_contiguous_subsum(arr)
+ret = []
+arr.each_with_index do |x,i|
+    arr.each_with_index do |y,j|
+        if j >= i
+            ret << arr[i..j]
+        end
+    end
+end
+
+final = ret.sort {|a,b| a.sum <=> b.sum}
+
+return final.last.sum
+
+end
+
+
+#The time complexity of this solution should be O(n^2)
+
+def linear_largest_contiguous_subsum(arr)
+
+    largest_sum = 0
+    current_sum = 0
+    arr.each_with_index do |x,i|
+        current_sum += arr[i]
+        if current_sum > largest_sum
+            largest_sum = current_sum
+        else current_sum = 0
+        end
+    end
+
+    return largest_sum
+    
+    
+end
+
+
+
+list = [5, 3, -7]
+p linear_largest_contiguous_subsum(list) # => 8
+
+# possible sub-sums
+[5]           # => 5
+[5, 3]        # => 8 --> we want this one
+[5, 3, -7]    # => 1
+[3]           # => 3
+[3, -7]       # => -4
+[-7]          # => -7
+
+list2 = [2, 3, -6, 7, -6, 7]
+p linear_largest_contiguous_subsum(list2) # => 8 (from [7, -6, 7])
+
+list3 = [-5, -1, -3]
+p linear_largest_contiguous_subsum(list3) # => -1 (from [-1])
+
